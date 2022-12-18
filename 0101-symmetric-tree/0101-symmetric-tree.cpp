d437963e7600cf6a8ecc9603a9bd13aadd4a0a18
Hussein Hany <hhany3997@gmail.com>
2022-12-19 00:00:17 +0200

    Time: 7 ms (75.44%), Space: 16.4 MB (78.86%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+
+void hussein() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    improve;
+}
+
+class Solution {
+public:
+    bool helper(TreeNode* one, TreeNode* two) {
+        if (!one && !two)
+        return true;
+        
+        if (!one || !two)
+        return false;
+        
+        if (one->val != two->val)
+        return false;
+        
+        return helper(one->left, two->right) && helper(one->right, two->left);
+    }
+    
+    bool isSymmetric(TreeNode *root) {
+        
+        if (!root)
+        return true;
+        
+        return helper(root->left, root->right);
+    }
+};
