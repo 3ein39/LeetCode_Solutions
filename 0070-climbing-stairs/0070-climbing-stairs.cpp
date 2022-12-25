50bd348f1ef6c531b99caaae7b5f14d460f7db29
Hussein Hany <hhany3997@gmail.com>
2022-12-25 16:27:29 +0200

    Time: 0 ms (100.00%), Space: 6 MB (39.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    long long dp[45 + 1];
+public:
+    int climbStairs(int n) {
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+    
+    long long fib(int n) {
+        if (n <= 2)
+            return 1;
+    
+        
+        return fib(n - 1) + fib(n - 2);
+    }
+};
