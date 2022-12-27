ac70e927e2aac5ea1de74d05c7f5e75e4b60187a
Hussein Hany <hhany3997@gmail.com>
2022-12-27 09:48:24 +0200

    Time: 21 ms (61.44%), Space: 22.9 MB (18.18%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+class Solution {
+public:
+    TreeNode *helper(int l, int r, vector<int> &nums) {
+        int mid = (l + r) / 2;
+        TreeNode *node = new TreeNode(nums[mid]);
+
+        if (l > r)
+            return nullptr;
+
+        node->left = helper(l, mid - 1, nums);
+        node->right = helper(mid + 1, r, nums);
+
+        return node;
+    }
+
+    TreeNode *sortedArrayToBST(vector<int> &nums) {
+        return helper(0, nums.size() - 1, nums);
+    }
+};
+
