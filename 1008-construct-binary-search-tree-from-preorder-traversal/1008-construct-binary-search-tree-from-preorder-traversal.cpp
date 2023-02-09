f401c6e26409703e0eb49c5df99115ed671e0948
Hussein Hany <hhany3997@gmail.com>
2023-02-09 12:15:06 +0200

    Time: 8 ms (44.58%), Space: 13.6 MB (77.30%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -21,40 +21,35 @@ int dy[4]{0, 1, 0, -1};
 /* 3ein39
                     " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
                                       My way to My dream....
-*/
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
+// */
+//  struct TreeNode {
+//      int val;
+//      TreeNode *left;
+//      TreeNode *right;
 
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//      TreeNode() : val(0), left(nullptr), right(nullptr) {}
 
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
+//      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  };
 
-// O(N^2) solution
+// O(N) solution
 class Solution {
 public:
-    TreeNode *helper(TreeNode* &root, int val) {
-        if (!root)
-            return root = new TreeNode(val);
-
-        if (val > root->val)
-            root->right = helper(root->right, val);
-        else
-            root->left = helper(root->left, val);
+    TreeNode *helper(vector<int>& preorder, int min, int max, int& i) {
+        if (i >= preorder.size() || preorder[i] < min || preorder[i] > max)
+            return nullptr;
+        TreeNode* root = new TreeNode(preorder[i++]);
+        
+        root->left = helper(preorder ,min, root->val, i);
+        root->right = helper(preorder, root->val, max, i);
         
         return root;
     }
 
     TreeNode *bstFromPreorder(vector<int> &preorder) {
-        TreeNode* root = nullptr;
-
-        for (auto &el: preorder)
-            helper(root, el);
-
-            return root;
+        int i = 0;
+        return helper(preorder, INT32_MIN, INT32_MAX, i);
     }
 };
