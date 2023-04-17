15ca3dbc5536caaf569cae95880f6560543f2034
Hussein Hany <hhany3997@gmail.com>
2023-04-17 16:53:12 +0200

    Time: 3 ms (60.69%), Space: 7.3 MB (53.18%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+const int OO = 10000000;
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+class Solution {
+public:
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for (int i = 0; i < pow(2, n); ++i) {
+            bitset<10> mask(i);
+            vector<int> subset;
+            for (int j = 0; j < n; ++j)
+                if (mask[j])
+                    subset.push_back(nums[j]);
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
+
+// int main() {
+//     hussein();
+//     Solution a;
+//     vector<int> v = {1,2,3};
+//     a.subsets(v);
+//     return 0;
+// }
