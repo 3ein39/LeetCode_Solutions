e2c6e0df7946daad1102498bb336eb12d7154bb0
Hussein Hany <hhany3997@gmail.com>
2023-07-15 17:57:17 +0300

    Time: 3 ms (99.77%), Space: 10.3 MB (84.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -15,44 +15,40 @@ using namespace std;
 #define no          cout<<"NO"<<endl
 #define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define Node        ListNode
-int dx[4]{-1, 0, 1, 0};
-int dy[4]{0, 1, 0, -1};
-const int OO = 10000000;
 
 /* 3ein39
                     " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
                                       My way to My dream....
 */
 
+
 void hussein() {
-    improve;
 #ifndef ONLINE_JUDGE
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 #endif
+    improve;
 }
+
+// n logn approach 
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis;
+        lis.pb(nums[0]);
+        
         int n = nums.size();
-        vector<int> memory(n+1, 1);
-
-        for (int i = n - 1; i > -1; --i)
-            for (int j = i + 1; j < n; ++j)
-                if (nums[i] < nums[j])
-                    memory[i] = max(memory[i], 1 + memory[j]);
-
-        return *max_element(all(memory));
+        
+        for (int i = 1; i < n; ++i) {
+            auto idx = lower_bound(all(lis), nums[i]) - lis.begin();
+            
+            if (idx == lis.size())
+                lis.pb(nums[i]);
+            else 
+                lis[idx] = nums[i];
+        }
+        return lis.size();
     }
 };
-// int main() {
-//     hussein();
 
-
-//     vector<int> v{10,9,2,5,3,7,101,18};
-//     Solution a;
-//     cout << a.lengthOfLIS(v);
-
-
-//     return 0;
-// }
