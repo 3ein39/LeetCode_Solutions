cb9f7b4248f173095843445c3c8d27e5fab07fa2
Hussein Hany <hhany3997@gmail.com>
2023-07-31 14:14:54 +0300

    Time: 34 ms (83.67%), Space: 10.9 MB (78.49%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define fi          first
+#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+#define endl        '\n';
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+    for (T &x: data)input >> x;
+    return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+    for (const T &x: data)output << x << " ";
+    return output;
+}
+
+class Solution {
+    string x, y;
+    int memo[1001][1001];
+public:
+    int dp(int i, int j) {
+        if (i == x.size() && j == y.size())return 0;
+
+        if (i == x.size()) {
+            int sum = 0;
+            for (int k = j; k < y.size(); ++k) sum += y[k];
+            return sum;
+        }
+
+        if (j == y.size()) {
+            int sum = 0;
+            for (int k = i; k < x.size(); ++k) sum += x[k];
+            return sum;
+        }
+        
+        auto& ret = memo[i][j];
+        if (~ret)
+            return ret;
+
+        if (x[i] == y[j])return ret = dp(i + 1, j + 1);
+        
+        return ret = min(x[i] + dp(i + 1, j), y[j] + dp(i, j + 1));
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        x = s1, y = s2;
+        memset(memo, -1, sizeof memo);
+        return dp(0,0);
+    }
+};
+
+// int main() {
+//     hussein();
+
+
+
+//     return 0;
+// }
