75cb2fb54b38a591c42bfe27a519db6e73b4bfe9
Hussein Hany <hhany3997@gmail.com>
2023-08-01 14:05:38 +0300

    Time: 24 ms (81.62%), Space: 9.9 MB (56.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define fi          first
+#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+#define endl        '\n';
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+    for (T &x: data)input >> x;
+    return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+    for (const T &x: data)output << x << " ";
+    return output;
+}
+
+class Solution {
+    vector<vector<int>> ans;
+    vector<int> cur;
+    int n, k;
+public:
+    void solve(int idx) {
+        if (cur.size() == k) {
+            ans.push_back(cur);
+            return;
+        }
+        for (int i = idx + 1; i <= n; ++i) {
+            cur.push_back(i);
+            solve(i);
+            cur.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n_, int k_) {
+        n = n_;
+        k = k_;
+
+        solve(0);
+        
+        return ans;
+    }
+};
+
+// int main() {
+//     hussein();
+
+
+
+//     return 0;
+// }
