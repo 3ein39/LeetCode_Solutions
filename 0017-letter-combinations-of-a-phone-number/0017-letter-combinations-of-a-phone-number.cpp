b18a74e34ffe5504ffb3363db84e063098262920
Hussein Hany <hhany3997@gmail.com>
2023-08-03 15:25:29 +0300

    Time: 3 ms (37.56%), Space: 6.6 MB (31.40%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define fi          first
+#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+#define endl        '\n';
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+    for (T &x: data)input >> x;
+    return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+    for (const T &x: data)output << x << " ";
+    return output;
+}
+
+class Solution {
+    int n;
+    string input;
+    map<char, string> mp;
+    vector<string> ans;
+    string cur;
+public:
+
+    void solve(int idx) {
+        if (idx == input.size()) {
+            ans.push_back(cur);
+            return;
+        }
+
+            string range = mp[input[idx]];
+            for (int j = 0; j < range.size(); ++j) {
+                cur.push_back(range[j]);
+                solve(idx + 1);
+                cur.pop_back();
+            }
+        }
+
+    vector<string> letterCombinations(string digits) {
+        n = digits.size();
+        
+        if (digits.empty())
+            return {};
+        
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        input = digits;
+        solve(0);
+
+        // watch(ans);
+        
+        return ans;
+    }
+};
+
+// int main() {
+    // hussein();
+// 
+    // return 0;
+// }
