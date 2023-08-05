bf9e9ace28567903bea01d49b5e6f8b2918292e9
Hussein Hany <hhany3997@gmail.com>
2023-08-05 14:28:59 +0300

    Time: 15 ms (82.91%), Space: 15.8 MB (68.53%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define allr(v)     (v).rbegin(), (v).rend()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+const int OO = 10000000;
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+  // struct TreeNode {
+  //     int val;
+  //     TreeNode *left;
+  //     TreeNode *right;
+  //     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  //     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  //     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  // };
+
+class Solution {
+public:
+
+    vector<TreeNode*> solve( int l, int r) {
+        if (l == r) {
+            return {new TreeNode(l)};
+        }
+        if (l > r) {
+            return {nullptr};
+        }
+        vector<TreeNode*> ans;
+        for (int i = l; i <= r; ++i) {
+            for (auto& lTree : solve(l, i - 1)) {
+                for (auto& Rtree : solve(i + 1, r)) {
+                    auto root  = new TreeNode(i, lTree, Rtree);
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0)
+            return {};
+
+        return solve( 1, n);
+    }
+};
+
+// int main() {
+//     hussein();
+
+
+
+//     return 0;
+// }
+
+
