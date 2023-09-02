a75bc98d8182b5fee4e06136cc5f8c2613a94853
Hussein Hany <hhany3997@gmail.com>
2023-09-02 08:40:51 +0300

    Time: 181 ms (54.15%), Space: 62.9 MB (87.56%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull         unsigned long long
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define allr(v)     (v).rbegin(), (v).rend()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+const int OO = 10000000;
+
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+    for (T &x: data)input >> x;
+    return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+    for (const T &x: data)output << x << " ";
+    return output;
+}
+
+void hussein() {
+    improve;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+class Solution {
+    set<string> words;
+    string s;
+    int dp[51];
+
+    int solve(int idx) {
+        if (idx == s.size()) return 0;
+
+        int& ret = dp[idx];
+        if (~ret) return ret;
+
+        ret = solve(idx + 1) + 1;
+        string cur;
+        int cnt = 0;
+        for (int i = idx; i < s.size(); ++i) {
+            cur += s[i];
+            if (words.count(cur)) {
+                ret = min(ret, solve(i + 1));
+            }
+        }
+
+        return ret;
+    }
+public:
+    int minExtraChar(string _s, vector<string>& dictionary) {
+        s = _s;
+        for (const string& word: dictionary) {
+            words.insert(word);
+        }
+
+        memset(dp, -1, sizeof dp);
+        return solve(0);
+    }
+};
