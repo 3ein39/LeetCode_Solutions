c9ff76a0ab89518c3c90963925bd05f1b12d6056
Hussein Hany <hhany3997@gmail.com>
2023-10-07 21:21:43 +0300

    Time: 0 ms (100.00%), Space: 12.7 MB (95.74%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull         unsigned long long
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+      for (T &x: data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x: data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+  // struct TreeNode {
+  //     int val;
+  //     TreeNode *left;
+  //     TreeNode *right;
+  //     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  //     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  //     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  // };
+
+class Solution {
+      vector<string> ans;
+   public:
+    void solve(TreeNode* node, string& state) {
+	  bool is_leaf = true;
+
+	  if (node->left) {
+	    state += "->" + to_string(node->left->val);
+	    is_leaf = false;
+	    solve(node->left, state);
+
+	    while(state.back() != '-' || state[state.size() - 2] == '>') state.pop_back();
+	    state.pop_back();
+	  }
+
+      if (node->right) {
+	    state += "->" + to_string(node->right->val);
+	    is_leaf = false;
+	    solve(node->right, state);
+
+        while(state.back() != '-' || state[state.size() - 2] == '>') state.pop_back();
+	    state.pop_back();
+      }
+
+	  if (is_leaf)
+	    ans.push_back(state);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+		string state = to_string(root->val);
+	    solve(root, state);
+	    return ans;
+    }
+};
+// int main() {
+// 	hussein();
+
+
+
+// 	return 0;
+// }
