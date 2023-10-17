d1b9ec2076247ef0e01bf81fb92a7529d5f5cb40
Hussein Hany <hhany3997@gmail.com>
2023-10-17 21:39:43 +0300

    Time: 3 ms (99.40%), Space: 11 MB (71.62%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -7,29 +7,26 @@ using namespace std;
 
 
 class Solution {
-    vector<int> path;
-    int n;
-    vector<vector<int>> ans;
 public:
-    void solve(vector<vector<int>> & graph, int node) {
-        if (node == n - 1) {
+    void solve(vector<vector<int>> & graph, int node, vector<vector<int>> &ans, vector<int>& path) {
+        if (node == graph.size() - 1) {
             ans.push_back(path);
             return;
         }
         
         for (auto& neighbour : graph[node]) {
             path.push_back(neighbour);
-            solve(graph, neighbour);
+            solve(graph, neighbour, ans, path);
             path.pop_back();
         }
     }
 
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        n = graph.size();
-        
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {        
+               vector<int> path;
         path.push_back(0);
-        
-        solve(graph, 0);
+        vector<vector<int>> ans;
+
+        solve(graph, 0, ans, path);
         
         return ans;
     }
