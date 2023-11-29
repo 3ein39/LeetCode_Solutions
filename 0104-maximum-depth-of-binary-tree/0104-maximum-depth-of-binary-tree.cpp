2261a6d5db3dfc5af256832abff2190439182665
Hussein Hany <hhany3997@gmail.com>
2023-11-29 13:12:28 +0200

    Time: 3 ms (91.50%), Space: 19.4 MB (8.88%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,24 +1,73 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
 
 // time  : O(n)
 // space : O(n)
 
 class Solution {
-public:
-    int maxDepth(TreeNode* root) {
+  public:
+	// bfs solution
+	int maxDepth(TreeNode* root) {
+        
         if (!root)
             return 0;
         
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
-    }
+		queue<TreeNode*> q;
+		q.push(root);
+		
+		int result = 0;
+		while (!q.empty()) {
+			int sz = q.size();
+			while (sz--) {
+				auto node = q.front();
+				q.pop();
+				
+				if (node->left)
+					q.push(node->left);
+
+				if (node->right)
+					q.push(node->right);
+			}
+			++result;
+		}
+		
+		return result;
+	}
 };
