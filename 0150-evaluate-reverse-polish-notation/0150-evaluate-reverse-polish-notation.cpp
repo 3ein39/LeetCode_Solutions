8246dab7dd4096b73a3e3e6c53d4a9873f54f224
Hussein Hany <hhany3997@gmail.com>
2023-12-24 20:15:43 +0200

    Time: 11 ms (36.90%), Space: 12.2 MB (67.31%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+class Solution {
+  public:
+	
+	bool isOperator(string& el) {
+		if (el == "+" || el == "-" || el == "*" || el == "/")
+			return true;
+		
+		return false;
+	}
+	
+	int eval(int a, int b, string op) {
+		if (op == "+")
+			return a + b;
+		else if (op == "-")
+			return a - b;
+		else if (op == "/")
+			return a / b;
+		else
+			return a * b;
+	}
+		
+	int evalRPN(vector<string> &tokens) {
+		stack<int> st;
+		for (auto &el : tokens) {
+			if (isOperator(el)) {
+				int a, b;
+				b = st.top(), st.pop();
+				a = st.top(), st.pop();
+				
+				st.push(eval(a, b, el));
+			} else {
+				st.push(stoi(el));
+			}
+		}
+		
+		return st.top();
+	}
+};
