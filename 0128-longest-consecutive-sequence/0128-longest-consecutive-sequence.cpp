69f84619b98f07b4efb781775984064f189ff547
Hussein Hany <hhany3997@gmail.com>
2023-12-24 12:43:22 +0200

    Time: 193 ms (40.93%), Space: 73 MB (15.00%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+class Solution {
+  public:
+	int longestConsecutive(vector<int>& nums) {
+		map<int, bool> freq;
+		for (auto& el : nums)
+			freq[el] = true;
+
+		int ans = 0, prev;
+		int mx = 0;
+		for (auto& [key, val]: freq) {
+			if (ans == 0 || key - prev == 1)
+				++ans, prev = key;
+			else
+				ans = 1, prev = key;
+			
+			mx = max(ans, mx);
+		}
+
+		return mx;
+	}
+};
