71a79693a11289be107feae2aa2410c8fa49d809
Hussein Hany <hhany3997@gmail.com>
2024-01-01 21:23:00 +0200

    Time: 24 ms (46.16%), Space: 17.8 MB (36.11%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+class Solution {
+  public:
+	int findContentChildren(vector<int>& g, vector<int>& s) {
+		sort(all(g)), sort(all(s));
+		int co = 0;
+		int n = g.size();
+		int ans = 0;
+		// for each child get valid cookie
+		for (int i = 0; i < n && co < s.size(); ++i) {
+			int greed = g[i];
+			while (co < s.size() && s[co] < greed)
+				co++;
+			if (co < s.size() && s[co] >= greed) ++ans, ++co;
+		}
+		
+		return ans;
+	}
+};
