ec91a470b18897aa3fdbf24b0e6091c62575011f
Hussein Hany <hhany3997@gmail.com>
2024-01-15 20:32:03 +0200

    Time: 0 ms (100.00%), Space: 17.5 MB (13.46%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+class Solution {
+		vector<int> subset, nums_;
+		vector<vector<int>> ans;
+		set<vector<int>> vis;
+		int n;
+  public:
+	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+		nums_ = nums;
+		n = nums.size();
+		solve();
+
+		return ans;
+	}
+
+	bool unique(vector<int> v) {
+		sort(all(v));
+		return !vis.count(v);
+	}
+
+	void solve(int i = 0) {
+		if (i >= n) {
+//			watch(subset);
+			if (unique(subset)) {
+				ans.pb(subset);
+				auto v = subset;
+				sort(all(v));
+				vis.insert(v);
+			}
+//				ans.pb(subset), vis.insert(subset);
+			return;
+		}
+
+		solve(i + 1 );
+		subset.pb(nums_[i]);
+		solve(i + 1);
+		subset.pop_back();
+	}
+};
