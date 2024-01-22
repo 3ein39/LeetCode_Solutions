9c4ee6d1022b3a83aa8e24410afd0ef3718977c3
Hussein Hany <hhany3997@gmail.com>
2024-01-22 22:38:03 +0200

    Time: 3 ms (68.25%), Space: 13.4 MB (5.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        int ans = -1;
+  public:
+	int search(vector<int>& nums, int target) {
+		int l = 0, r = nums.size() - 1;
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
 
-    while (l <= r) {
-        int mid = l + (r - l) / 2;
+			if (nums[mid] == target)
+				return mid;
 
-        if (nums[mid] == target) return mid;
-        else if (nums[l] <= nums[mid]) {
-            if (target >= nums[l] && target <= nums[mid])
-                r = mid - 1;
-            else l = mid + 1;
-        } else {
-            // sorted in right half
-            if (nums[r] >= target && nums[mid] <= target) l = mid + 1;
-            else r = mid - 1;
-        }
-    }
-        return ans;
-    }
+			if (nums[l] <= nums[mid]) {
+				// sorted asc
+				if (target >= nums[l] && target <= nums[mid])
+					r = mid - 1;
+				else
+					l = mid + 1;
+			}
+			else {
+				// sorted des
+				if (target <= nums[r] && target >= nums[mid])
+					l = mid + 1;
+				else
+					r = mid - 1;
+			}
+		}
+		return -1;
+	}
 };
