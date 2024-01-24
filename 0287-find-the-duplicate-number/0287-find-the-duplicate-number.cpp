0f385b52bc6153883338b5dcf164ae21c2a96ac9
Hussein Hany <hhany3997@gmail.com>
2024-01-24 22:57:19 +0200

    Time: 67 ms (96.75%), Space: 89.8 MB (9.45%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,85 +1,13 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define ull         unsigned long long
-#define ll          long long
-#define lp(n)       for(ll i = 0; i < (n); ++i)
-#define lp1(n)      for(ll i = 1; i <= (n); ++i)
-#define tc          ll testcase;   cin>>testcase;   while(testcase--)
-#define all(v)      (v).begin(), (v).end()
-#define watch(num)  cout<< #num <<": "<< num << "\n";
-#define allr(v)     (v).rbegin(), (v).rend()
-//#define fi          first
-//#define se          second
-#define pb          push_back
-#define yes         cout<<"YES"<<endl
-#define no          cout<<"NO"<<endl
-#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-int dx[4]{-1, 0, 1, 0};
-int dy[4]{0, 1, 0, -1};
-const int OO = 10000000;
-
-
-template<typename T>
-istream &operator>>(istream &input, vector<T> &data) {
-    for (T &x: data)input >> x;
-    return input;
-}
-
-template<typename T>
-ostream &operator<<(ostream &output, const vector<T> &data) {
-    for (const T &x: data)output << x << " ";
-    return output;
-}
-
-void hussein() {
-    improve;
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-}
-/* 3ein39
-                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
-                                      My way to My dream....
-*/
-
 class Solution {
-    int n;
-public:
+  public:
+	int findDuplicate(vector<int>& nums) {
+		vector<int> freq(1e5+1, 0);
+		for (auto& el : nums) freq[el]++;
 
-    int advance(int i) {
-        i++;
-        if (i == n)
-            i = 0;
-        return i;
-    }
-    int findDuplicate(vector<int>& nums) {
-        int slow = 0;
-        int fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
+		for (int i = 0; i < freq.size(); ++i) {
+			if (freq[i] > 1) return i;
+		}
 
-        slow = 0;
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
+		return 0;
+	}
 };
-
-
-// int main() {
-//     hussein();
-
-// vector<int> v =
-//         {18,13,14,17,9,19,7,17,4,6,17,5,11,10,2,15,8,12,16,17};
-//     cout << Solution().findDuplicate(v);
-
-//     return 0;
-// }
