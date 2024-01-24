207e7354df41a46d1d06199a80476ac10651d801
Hussein Hany <hhany3997@gmail.com>
2024-01-24 23:02:41 +0200

    Time: 7 ms (22.28%), Space: 13.3 MB (6.58%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define watch(num)  cout<< #num <<": "<< num << "\n";
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data)input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data)output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+
+//  struct ListNode {
+//       int val;
+//       ListNode *next;
+//       ListNode() : val(0), next(nullptr) {}
+//       ListNode(int x) : val(x), next(nullptr) {}
+//       ListNode(int x, ListNode *next) : val(x), next(next) {}
+//   };
+class Solution {
+  public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		int cnt = 0;
+		ListNode* temp = head;
+		while (temp != nullptr) {
+			cnt++;
+			temp = temp->next;
+		}
+		
+		temp = head;
+		ListNode* prev = nullptr;
+		for (int i = 0; i < cnt - n; ++i) {
+			prev = temp;
+			temp = temp->next;
+		}
+		if (prev == nullptr) {
+			head = head->next;
+		} else {
+			prev->next = temp->next;
+		}
+		return head;
+	}
+};
