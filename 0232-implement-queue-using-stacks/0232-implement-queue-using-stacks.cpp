a7c20db40d4f6133362609ba5cd8210b68bb690e
Hussein Hany <hhany3997@gmail.com>
2024-01-29 21:52:12 +0200

    Time: 0 ms (100.00%), Space: 8.3 MB (12.38%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+//#define fi          first
+//#define se          second
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define Node        ListNode
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+class MyQueue {
+    stack<int> s1;
+    stack<int> s2;
+
+    void move(stack<int> &from, stack<int> &to) {
+        while (!from.empty()) {
+            to.push(from.top());
+            from.pop();
+        }
+    }
+
+public:
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+        move(s1, s2);
+        s1.push(x);
+        move(s2, s1);
+    }
+
+    int pop() {
+        int val = s1.top();
+        s1.pop();
+        return val;
+    }
+
+    int peek() {
+        return s1.top();
+    }
+
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
