c85029592717650ebf9152c2f66330ff0ed8680c
Hussein Hany <hhany3997@gmail.com>
2024-02-15 14:54:49 +0200

    Time: 1521 ms (10.06%), Space: 654.6 MB (8.10%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+using namespace std;
+
+#define ll long long
+#define lp(n) for (ll i = 0; i < (n); ++i)
+#define lp1(n) for (ll i = 1; i <= (n); ++i)
+#define tc         \
+  ll testcase;     \
+  cin >> testcase; \
+  while (testcase--)
+#define all(v) (v).begin(), (v).end()
+#define allr(v) (v).rbegin(), (v).rend()
+#define watch(num) cout << #num << ": " << num << "\n";
+#define pb push_back
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define improve                     \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data) input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data) output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+class WordDictionary {
+    public:
+	// hashmap for storing the graph
+	unordered_map<int, WordDictionary*> tree;
+	bool isLeaf;
+	WordDictionary() {
+		isLeaf = false;
+	}
+
+	void addWord(string word) {
+		insert(word);
+	}
+
+	void insert(string word, int idx = 0) {
+		// base case
+		if (idx == word.size()) {
+			isLeaf = true;
+		}
+		else {
+			// ascii of the char
+			int char_ascii = word[idx] - 'a';
+			if (!tree.count(char_ascii))
+				tree[char_ascii] = new WordDictionary();
+			tree[char_ascii]->insert(word, idx + 1);
+		}
+	}
+
+	bool search(string word, int idx = 0) {
+// base case
+		if (idx == word.size()) {
+			return isLeaf;
+		}
+		// if the char is '.'
+		if (word[idx] == '.') {
+			for (auto &it : tree) {
+				if (it.second->search(word, idx + 1))
+					return true;
+			}
+			return false;
+		}
+		// ascii of the char
+		int char_ascii = word[idx] - 'a';
+		if (!tree.count(char_ascii))
+			return false;
+		return tree[char_ascii]->search(word, idx + 1);
+	}
+};
+
