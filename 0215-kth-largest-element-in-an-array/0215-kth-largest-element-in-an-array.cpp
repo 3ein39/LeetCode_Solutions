8513760a3bf4abee5d2650a0b2418a16edbfcf57
Hussein Hany <hhany3997@gmail.com>
2024-02-18 16:26:47 +0200

    Time: 101 ms (52.18%), Space: 64.3 MB (7.29%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,40 +1,61 @@
 #include <bits/stdc++.h>
 
+#include <utility>
 using namespace std;
 
-#define ll          long long
-#define lp(n)       for(ll i = 0; i < (n); ++i)
-#define lp1(n)      for(ll i = 1; i <= (n); ++i)
-#define tc          ll testcase;   cin>>testcase;   while(testcase--)
-#define all(v)      (v).begin(), (v).end()
-#define allr(v)     (v).rbegin(), (v).rend()
-//#define fi          first
-//#define se          second
-#define pb          push_back
-#define yes         cout<<"YES"<<endl
-#define no          cout<<"NO"<<endl
-#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define Node        ListNode
-int dx[4]{-1, 0, 1, 0};
-int dy[4]{0, 1, 0, -1};
-const int OO = 10000000;
+#define ll long long
+#define lp(n) for (ll i = 0; i < (n); ++i)
+#define lp1(n) for (ll i = 1; i <= (n); ++i)
+#define tc         \
+  ll testcase;     \
+  cin >> testcase; \
+  while (testcase--)
+#define all(v) (v).begin(), (v).end()
+#define allr(v) (v).rbegin(), (v).rend()
+#define watch(num) cout << #num << ": " << num << "\n";
+#define pb push_back
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define improve                     \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-/* 3ein39
-                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
-                                      My way to My dream....
-*/
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data) input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data) output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
 class Solution {
-public:
-    int findKthLargest(vector<int> &nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> min_heap;
-        for (auto &el: nums) {
-            min_heap.push(el);
-        }
-        while (min_heap.size() > k) min_heap.pop();
-        
-        return min_heap.top();
-    }
-
-    
+  public:
+	int findKthLargest(vector<int>& nums, int k) {
+		priority_queue<int> q;
+		for (auto& el : nums)
+			q.push(el);
+		
+        k -= 1;
+		while (k--)
+			q.pop();
+		
+		return q.top();
+	}
 };
-
