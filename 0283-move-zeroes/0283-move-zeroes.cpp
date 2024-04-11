629ef3dbdc5c2dc6d888f4aa8bc422ca1fd8edc7
Hussein Hany <hhany3997@gmail.com>
2024-04-11 19:25:26 +0200

    Time: 19 ms (36.51%), Space: 22.8 MB (5.42%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#include <utility>
+using namespace std;
+
+#define ll long long
+#define lp(n) for (ll i = 0; i < (n); ++i)
+#define lp1(n) for (ll i = 1; i <= (n); ++i)
+#define tc         \
+  ll testcase;     \
+  cin >> testcase; \
+  while (testcase--)
+#define all(v) (v).begin(), (v).end()
+#define allr(v) (v).rbegin(), (v).rend()
+#define watch(num) cout << #num << ": " << num << "\n";
+#define pb push_back
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define improve                     \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+int dx4[4]{-1, 0, 1, 0};
+int dy4[4]{0, 1, 0, -1};
+int dx8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+template<typename T>
+istream &operator>>(istream &input, vector<T> &data) {
+	for (T &x : data) input >> x;
+	return input;
+}
+
+template<typename T>
+ostream &operator<<(ostream &output, const vector<T> &data) {
+	for (const T &x : data) output << x << " ";
+	return output;
+}
+
+void hussein() {
+	improve;
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+
+class Solution {
+  public:
+	void moveZeroes(vector<int>& nums) {
+		int zeros = 0;
+		for (auto& el : nums)
+			zeros += (el == 0);
+		vector<int> ans;
+		for (auto& el : nums) {
+			if (el == 0) continue;
+
+			ans.pb(el);
+		}
+		while (zeros--)
+			ans.pb(0);
+		// cout << ans;
+		for (int i = 0; i < nums.size(); ++i) {
+			nums[i] = ans[i];
+		}
+	}
+};
+// int main() {
+// 	hussein();
+
+
+// 	return 0;
+// }
