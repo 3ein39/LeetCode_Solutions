02c7e658de5fe67a39a85ee29687f5da4b102757
Hussein Hany <hhany3997@gmail.com>
2024-04-26 21:17:34 +0300

    Time: 6 ms (80.94%), Space: 9.8 MB (5.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        return s == t;
-    }
+  public:
+	bool isAnagram(string s, string t) {
+		unordered_map<char, int> mp;
+		for (auto& el : s)
+			mp[el]++;
+		for (auto & el: t) {
+			if (mp[el] <= 0)
+				return false;
+			mp[el]--;
+		}
+		
+		for (auto& p : mp)
+			if (p.second)
+				return false;
+		
+		return true;
+	}
 };
